773c4b8f6c551f0a89637ad4c70b756bf4d9dc37
Gabriel Oliveira Goulart <gabrielgoulart2001@gmail.com>
2023-10-08 17:02:22 -0300

    dias do calendario com icone de movimentação

--- src/componentes/Calendario/Calendario.tsx
+++ src/componentes/Calendario/Calendario.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 import classNames from 'classnames';
 import "moment/locale/pt-br";
 import "./CalendarioStyle.scss";
+import { MovimentacaoService } from "../../services/MovimentacaoService";
+import back from "../../http";
+import { IMovimentacao } from "../../interfaces/IMovimentacao";
+import { AddCircleOutlineRounded, Circle, RemoveCircleOutlineRounded } from "@mui/icons-material";
+import { CircularProgress } from "@mui/material";
 
 interface CalendarioProps {
 	onDayClick: (selectedDate: Date) => void;
@@ -12,16 +17,42 @@ const Calendario: React.FC<CalendarioProps> = ({ onDayClick }) => {
 	moment.locale("pt-br");
 	const [currentMonth, setCurrentMonth] = useState(moment());
 	const [selectedDay, setSelectedDay] = useState(moment().date() + currentMonth.format('YYYYMM'));
-
 	const weekdays = moment.weekdaysShort();
 	const firstDayOfMonth = currentMonth.clone().startOf("month");
 	const daysInMonth = currentMonth.daysInMonth();
 	const startingDay = parseInt(firstDayOfMonth.format("d"), 10);
+	const [movimentacoesDoMes, setMovimentacoesDoMes] = useState<IMovimentacao[]>([]);
+	const [carregado, setCarregado] = useState(false);
 
 	const days = [];
 	for (let i = 0; i < startingDay; i++) {
 		days.push(<div key={i} className="empty-day"></div>);
 	}
+
+	useEffect(() => {
+		const buscaMovimentacoesDoMes = async () => {
+			try {
+				console.log("atualizou movimentações");
+				const movimentacaoService = new MovimentacaoService(back);
+				const primeiroDiaMes = new Date();
+				primeiroDiaMes.setMonth(currentMonth.month());
+				primeiroDiaMes.setFullYear(currentMonth.year());
+				primeiroDiaMes.setDate(1);
+				const ultimoDiaMes = new Date();
+				ultimoDiaMes.setMonth(ultimoDiaMes.getMonth() + 1);
+				ultimoDiaMes.setDate(0);
+				const response = await movimentacaoService.getMovimentacao(1,
+					primeiroDiaMes.getTime(), ultimoDiaMes.getTime());
+				if (response?.data) {
+					setMovimentacoesDoMes(response.data);
+					setCarregado(true);
+				}
+			} catch (error) {
+				console.error('Erro ao buscar movimentações:', error);
+			}
+		};
+		buscaMovimentacoesDoMes();
+	}, [selectedDay])
 
 	for (let day = 1; day <= daysInMonth; day++) {
 		const isCurrentDay = (day + currentMonth.format('YYYYMM')) === selectedDay;
@@ -34,7 +65,9 @@ const Calendario: React.FC<CalendarioProps> = ({ onDayClick }) => {
 				className={dayClass}
 				onClick={() => handleDayClick(day, currentMonth.month(), currentMonth.year())}
 			>
+				{verificaDiaComMovimentação(day, currentMonth.month(), "NEGATIVO")}
 				{day}
+				{verificaDiaComMovimentação(day, currentMonth.month(), "POSITIVO")}
 			</div>
 		);
 	}
@@ -48,43 +81,86 @@ const Calendario: React.FC<CalendarioProps> = ({ onDayClick }) => {
 		onDayClick(selectedDate);
 	};
 
-
 	return (
-		<div className="calendar-card">
-			<div className="header">
-				<button onClick={() =>
-					<div>
-						{setCurrentMonth(currentMonth.clone().subtract(1, "month"))}
-						{handleDayClick(1, currentMonth.month() -1, currentMonth.year())}
-					</div>
-				}
-				>Anterior</button>
-				<h1>{currentMonth.format("MMMM YYYY")}</h1>
-				<button onClick={() =>
-				<div>
-					{setCurrentMonth(currentMonth.clone().add(1, "month"))}
-					{handleDayClick(1, currentMonth.month() + 1, currentMonth.year())}
+		carregado
+			? <div className="calendar-card">
+				<div className="header">
+					<button onClick={() =>
+						<div>
+							{setCurrentMonth(currentMonth.clone().subtract(1, "month"))}
+							{handleDayClick(1, currentMonth.month() - 1, currentMonth.year())}
+						</div>
+					}
+					>Anterior</button>
+					<h1>{currentMonth.format("MMMM YYYY")}</h1>
+					<button onClick={() =>
+						<div>
+							{setCurrentMonth(currentMonth.clone().add(1, "month"))}
+							{handleDayClick(1, currentMonth.month() + 1, currentMonth.year())}
+						</div>
+
+					}
+					>Próximo</button>
 				</div>
-					
-				}
-				>Próximo</button>
+				<div className="weekdays">
+					{weekdays.map((weekday) => (
+						<div key={weekday} className="weekday">
+							{weekday}
+						</div>
+					))}
+				</div>
+				<div className="days">{days}</div>
 			</div>
-			<div className="weekdays">
-				{weekdays.map((weekday) => (
-					<div key={weekday} className="weekday">
-						{weekday}
-					</div>
-				))}
+			: <div>
+				<CircularProgress color="inherit" />
 			</div>
-			<div className="days">{days}</div>
-		</div>
 	);
 
-	function verificaDiaComMovimentação(day: number, month: number, year: number) {
-		const date = new Date();
-		date.setDate(day);
-		date.setMonth(month);
-		date.setFullYear(year);
+	function verificaDiaComMovimentação(day: number, month: number, operador: string) {
+		if (movimentacoesDoMes.length > 0) {
+			let possuiMovimentacao = 0;
+			movimentacoesDoMes.forEach((movimentacao) => {
+				console.log("dias", new Date(movimentacao.dataMovimentacao).getDate(), day);
+				let date = new Date(movimentacao.dataMovimentacao)
+				const dia = date.getDate();
+				const mes = date.getMonth();
+				if (dia === day && month === mes
+					&& movimentacao.tipoMovimentacao.toUpperCase() === operador) {
+					possuiMovimentacao++;
+				}
+			});
+			switch (operador) {
+				case "NEGATIVO":
+					if (possuiMovimentacao > 0) {
+						console.log("POSSUI");
+						return (
+							<Circle
+								sx={{ color: "#9a1d1d", backgroundColor: 'transparent' }}
+								fontSize='medium'
+							/>
+						);
+					}
+					return (
+						<Circle
+							sx={{ color: "transparent", backgroundColor: 'transparent' }}
+						/>
+					);
+				case "POSITIVO":
+					if (possuiMovimentacao > 0) {
+						return (
+							<Circle
+								sx={{ color: "#35b13b", backgroundColor: 'transparent' }}
+								fontSize='medium'
+							/>
+						);
+					}
+					return (
+						<Circle
+							sx={{ color: "transparent", backgroundColor: 'transparent' }}
+						/>
+					);
+			}
+		}
 	}
 };
 
