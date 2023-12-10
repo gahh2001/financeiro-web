ad4be3f2730a68546715ba8dc30b15499a38bbeb
Gabriel Oliveira Goulart <gabrielgoulart2001@gmail.com>
2023-12-10 16:22:44 -0300

    add validação de inputs ao salvar

--- src/componentes/ModalAddMovimentacao/ModalAddMovimentacao.tsx
+++ src/componentes/ModalAddMovimentacao/ModalAddMovimentacao.tsx
@@ -8,8 +8,9 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import dayjs, { Dayjs } from 'dayjs';
 import 'dayjs/locale/en-gb';
-import { ReactNode, useEffect, useState } from "react";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
 import { TipoMovimentacaoEnum } from "../../enums/TipoMovimentacaoEnum";
 import back from '../../http';
 import { ICategoriaMovimentacao } from '../../interfaces/ICategoriaMovimentacao';
@@ -36,14 +37,20 @@ export default function ModalAddMovimentacao(props: ModalType) {
 	const tipoMovimentacao = props.tipo === TipoMovimentacaoEnum.POSITIVO
 		? 'rendimento' : 'despesa'
 	const [categoriasCarregadas, setCategoriasCarregadas] = useState<ICategoriaMovimentacao[]>([]);
+	const [data, setData] = useState<Dayjs | null>(dayjs());
 	const [categoria, setCategoria] = useState('');
 	const [valor, setValor] = useState('');
-	const [data, setData] = useState<Date>();
+	const [descricao, setDescricao] = useState("");
+	const [emptyForm, setEmptyForm] = useState(false);
 
 	const handleChangeCategoria = (event: SelectChangeEvent) => {
 		const newValue = event.target.value;
 		setCategoria(typeof newValue === 'string' ? newValue : '');
 	};
+
+	const handleChangeDescricao = (event: ChangeEvent<HTMLInputElement>) => {
+		setDescricao(event.target.value);
+	}
 
 	const convertInputValor = (event: any) => {
 		let value = event.target.value;
@@ -55,6 +62,7 @@ export default function ModalAddMovimentacao(props: ModalType) {
 	useEffect( () => {
 		setValor("");
 		setCategoria("");
+		setEmptyForm(false);
 		const buscaCategorias = async () => {
 			try {
 				const categorias = await categoriaMovimentacaoService
@@ -74,58 +82,76 @@ export default function ModalAddMovimentacao(props: ModalType) {
 			{props.isOpen && (
 				<div className="modal-overlay-adiciona">
 					<div className="modal-adiciona">
-						<div className="titulo">Adicionar {tipoMovimentacao}</div>
-							<ThemeProvider theme={darkTheme}>
-								<div className='inputs'>
-									<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
-										<DemoContainer components={['DatePicker']}>
-											<DatePicker
-												sx={{ m: 1, width: "25vh" }}
-												label="Data"
-												onChange={(value) => setSelectedDate(value)}
+						<form>
+							<div className="titulo">Adicionar {tipoMovimentacao}</div>
+								<ThemeProvider theme={darkTheme}>
+									<div className='inputs'>
+										<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+											<DemoContainer components={['DatePicker']}>
+												<DatePicker
+													sx={{ m: 1, width: "25vh" }}
+													label="Data"
+													value={data}
+													onChange={(newValue) => setData(newValue)}
+												/>
+											</DemoContainer>
+										</LocalizationProvider>
+										<div className='space'></div>
+										<FormControl
+											required
+											sx={{ m: 1, width: "20vh" }}
+											error={emptyForm}
+										>
+											<InputLabel
+												id="demo-simple-select-helper-label"
+											>
+												Categoria
+											</InputLabel>
+											<Select
+												id="select-categoria"
+												value={categoria}
+												label="Age"
+												onChange={handleChangeCategoria}
+												required={true}
+											>
+											{obtemSelectCategorias(categoriasCarregadas)}
+											</Select>
+										</FormControl>
+										<TextField
+											required
+											error={emptyForm}
+											value={valor}
+											onChange={convertInputValor}
+											inputProps={{ type: 'number', step: "0.5"}}
+											sx={{ m: 1, width: "18vh" }}
+											label= "Valor"
+										/>
+									</div>
+									<div className='input-descricao'>
+										<Box
+											sx={{width: "98.5%"}}
+										>
+											<TextField
+												fullWidth
+												label="Escreva alguma observação sobre a movimentação"
+												id="fullWidth"
+												onChange={handleChangeDescricao}
 											/>
-										</DemoContainer>
-									</LocalizationProvider>
-									<div className='space'></div>
-									<FormControl sx={{ m: 1, width: "20vh" }}>
-										<InputLabel
-											id="demo-simple-select-helper-label"
-										>
-											Categoria
-										</InputLabel>
-										<Select
-											id="select-categoria"
-											value={categoria}
-											label="Age"
-											onChange={handleChangeCategoria}
-										>
-										{obtemSelectCategorias(categoriasCarregadas)}
-										</Select>
-									</FormControl>
-									<TextField
-										value={valor}
-										onChange={convertInputValor}
-										inputProps={{ type: 'number', step: "0.5"}}
-										sx={{ m: 1, width: "18vh" }}
-										label= "Valor"
-									/>
-								</div>
-								<div className='input-descricao'>
-									<Box
-										sx={{width: "98.5%"}}
-									>
-										<TextField fullWidth label="Descrição" id="fullWidth" />
-									</Box>
-								</div>
-							</ThemeProvider>
-						<div className="buttons">
-							<button onClick={props.closeModal}>
-								Cancelar
-							</button>
-							<button onClick={() => teste()}>
-								Salvar
-							</button>
-						</div>
+										</Box>
+									</div>
+								</ThemeProvider>
+							<div className="buttons">
+								<button onClick={props.closeModal}>
+									Cancelar
+								</button>
+								<button
+									onClick={() => teste()}
+									type="submit"
+								>
+									Salvar
+								</button>
+							</div>
+						</form>
 					</div>
 				</div>
 			)}
@@ -133,7 +159,17 @@ export default function ModalAddMovimentacao(props: ModalType) {
 	);
 
 	function teste() {
-		console.log(selectedDate);
+		const emptyFields = data === undefined
+			|| categoria === undefined || categoria === ""
+			|| valor === undefined || valor === ""
+			|| descricao === undefined || descricao === ""
+		if (emptyFields) {
+			setEmptyForm(true)
+		}
+		console.log(data);
+		console.log(categoria);
+		console.log(valor);
+		console.log(descricao);
 	}
 
 	function obtemSelectCategorias(categoriasReceived: ICategoriaMovimentacao[]) {
