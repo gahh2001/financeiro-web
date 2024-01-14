1d1dbe88d092449a28d3fdc7e41ad0a956a8d931
Gabriel Oliveira Goulart <gabrielgoulart2001@gmail.com>
2024-01-14 20:25:27 -0300

    cria redirecionamentos para páginas

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './paginas/Home/Home';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
 	<BrowserRouter>
