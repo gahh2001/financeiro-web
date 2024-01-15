c3a89f4810654afb86b6883050406455ed9a0ad5
Gabriel Oliveira Goulart <gabrielgoulart2001@gmail.com>
2024-01-14 21:22:35 -0300

    add página Analítico

---
+++ src/paginas/analitico/Analitico.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../../componentes/AppBar/AppBar";
+
+
+export const Analitico = () => {
+
+
+	return (
+		<>
+			<NavBar/>
+			<div className="analitico">
+				kkkkk
+			</div>
+		</>
+	);
+}
+
+export default Analitico;
