2512d74eb0d8720a2b6a5e815db905635ae728de
Gabriel Oliveira Goulart <gabrielgoulart2001@gmail.com>
2024-02-13 00:04:30 -0300

    adiciona graficos analiticos

--- src/componentes/analitico/categoriasVisaoGeral/CategoriasVisaoGeral.tsx
+++ src/componentes/analitico/categoriasVisaoGeral/CategoriasVisaoGeral.tsx
@@ -1,17 +1,23 @@
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import { BarChart } from '@mui/x-charts/BarChart';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import dayjs, { Dayjs } from 'dayjs';
 import { FC, useState } from 'react';
+import { TipoMovimentacaoEnum } from '../../../enums/TipoMovimentacaoEnum';
 import '../../../paginas/analitico/Analitico.scss';
 import './CategoriasVisaoGeral.scss';
 
 const CategoriasVisaoGeral: FC = () => {
 	const [nomeCategorias, setNomeCategorias] = useState<string[]>(["teste", "teste1", "teste3", "teste4", "teste2"]);
 	const [somaCategorias, setSomaCategorias] = useState<number[]>([2, 4, 3, 4, 3.5]);
-	const [periodo, setPeriodo] = useState("Mês");
+	const [ano, setAno] = useState<Dayjs | null>(dayjs());
+	const [categoria, setCategoria] = useState("");
 
-	const handleChangePeriodo = (event: SelectChangeEvent) => {
+	const handleChangeCategoria = (event: SelectChangeEvent) => {
 		const newValue = event.target.value;
-		setPeriodo(typeof newValue === 'string' ? newValue : "");
+		setCategoria(typeof newValue === 'string' ? newValue : "");
 	};
 
 	return (
@@ -19,30 +25,53 @@ const CategoriasVisaoGeral: FC = () => {
 			<div className="titulo">
 				Visão geral
 			</div>
-			<div className="filter">
-				<FormControl
-					sx={{ width: "20vh", height: "4%" }}
-				>
-					<InputLabel
-						id="demo-simple-select-helper-label"
-					>
-						Período
-					</InputLabel>
-					<Select
-						id="select-periodo"
-						value={periodo}
-						onChange={handleChangePeriodo}
-						required={true}
-					>
-						<MenuItem
-							key={"mes"}
-							value="MES"
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DemoContainer components={['DatePicker']}>
+					<div className="filter">
+						<DatePicker
+							label={"Ano"}
+							sx={{m: 1, width: "20vh"}}
+							slotProps={{ textField: { size: 'small' } }}
+							format='YYYY'
+						/>
+					</div>
+					<div className="filter">
+						<DatePicker
+							label={"Mês"}
+							views={['month']}
+							sx={{m: 1, width: "20vh"}}
+							slotProps={{ textField: { size: 'small' } }}
+							format='MM'
+						/>
+					</div>
+					<div className="filter">
+						<FormControl
+							sx={{ m: 1, width: "20vh" }}
+							size='small'
 						>
-							Mês
-						</MenuItem>
-					</Select>
-				</FormControl>
-			</div>
+							<InputLabel
+								id="demo-simple-select-helper-label"
+							>
+								Categorias
+							</InputLabel>
+							<Select
+								id="select-categoria"
+								value={categoria}
+								label="Age"
+								onChange={handleChangeCategoria}
+								required={true}
+							>
+								<MenuItem
+									key={"POSITIVAS"}
+									value={TipoMovimentacaoEnum.POSITIVO}
+								>
+									Positivas
+								</MenuItem>
+							</Select>
+						</FormControl>
+					</div>
+				</DemoContainer>
+			</LocalizationProvider>
 			<div className='grafic'>
 				<BarChart
 					xAxis={[
@@ -62,7 +91,7 @@ const CategoriasVisaoGeral: FC = () => {
 						left: 45,
 						right: 20,
 						top: 20,
-						bottom: 25,
+						bottom: 35,
 					}}
 				/>
 			</div>
