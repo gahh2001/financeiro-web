67e934446ac8cd1c42c65d4fa8b505503b110282
gabriel.goulart <gabriel.goulart@syonet.com>
2025-02-09 17:29:55 -0300

    refatoração tokens

--- src/http/index.tsx
+++ src/http/index.tsx
@@ -1,7 +1,26 @@
 import axios from "axios";
 
-const back = axios.create({
-	baseURL: 'https://mywalletpro.com.br/api'
-});
+export const useBack = () => {
 
-export default back
+	const instance = axios.create({
+		baseURL: 'https://mywalletpro.com.br/api',
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	instance.interceptors.request.use(
+		(config) => {
+			const token = localStorage.getItem("accessToken"); 
+			if (token) {
+				config.headers.Authorization = `Bearer ${token}`;
+			}
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
+
+	return instance;
+};
