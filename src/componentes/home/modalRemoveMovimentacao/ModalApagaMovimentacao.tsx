754505471d947ed99feff67edb2692dc1cea3c38
gabriel.goulart <gabriel.goulart@syonet.com>
2025-03-03 15:58:55 -0300

    ajusta modal remove movimentações para dialog

--- src/componentes/home/modalRemoveMovimentacao/ModalApagaMovimentacao.tsx
+++ src/componentes/home/modalRemoveMovimentacao/ModalApagaMovimentacao.tsx
@@ -1,7 +1,4 @@
-import { DeleteForever } from '@mui/icons-material';
-import CheckIcon from '@mui/icons-material/Check';
-import { LinearProgress } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Button, Dialog, DialogActions, DialogContent } from '@mui/material';
 import { FC, useEffect, useState } from "react";
 import { TipoMovimentacaoEnum } from '../../../enums/TipoMovimentacaoEnum';
 import { useBack } from '../../../http';
@@ -13,7 +10,6 @@ import "./ModalApagaMovimentacao.scss";
 const ModalApagaMovimentacao: FC<IModalApagar> = (props: IModalApagar) => {
 	const movimentacaoService = new MovimentacaoService(useBack());
 	const [success, setSuccess] = useState(false);
-	const [loading, setLoading] = useState(false);
 	const { showAlert, showError } = useAlert();
 	const tipoMovimentacao = props.tipo === TipoMovimentacaoEnum.POSITIVO ? 'rendimento' : 'despesa';
 	const possuiMovimentacaoEData = props?.movimentacao !== undefined
@@ -44,67 +40,54 @@ const ModalApagaMovimentacao: FC<IModalApagar> = (props: IModalApagar) => {
 	}, [props.closeModalRemove])
 
 	return (
-		<>
-			{props.isOpen && possuiMovimentacaoEData && !!date &&(
-				<div className="modal-overlay-apaga">
-					<div className="modal-apaga">
-						<div className="titulo">Apagar {tipoMovimentacao}</div>
-						<div className="aviso">
-							Deseja mesmo apagar a seguinte movimentação:
+		<Dialog
+			open={props.isOpen}
+			onClose={props.closeModalRemove}
+		>
+			<DialogContent>
+				<div className="modal-apaga">
+					<div className="titulo">Apagar {tipoMovimentacao}</div>
+					<div className="aviso">
+						Deseja mesmo apagar a seguinte movimentação:
+					</div>
+					<div className="headers">
+						<p>Data</p>
+						<p>Categoria</p>
+						<p>Valor</p>
+					</div>
+					<div className="movimentacao">
+						<div className="info">
+							{date?.getDate().toString().padStart(2,"0")}
+							/{date && date?.getMonth() + 1}
+							/{date?.getFullYear()}
 						</div>
-						<div className="headers">
-							<p>Data</p>
-							<p>Categoria</p>
-							<p>Valor</p>
+						<div className="info">
+							{props.movimentacao?.nomeCategoriaMovimentacao}
 						</div>
-						<div className="movimentacao">
-							<div className="info">
-								{date?.getDate().toString().padStart(2,"0")}
-								/{date.getMonth() + 1}
-								/{date?.getFullYear()}
-							</div>
-							<div className="info">
-								{props.movimentacao?.descricaoMovimentacao}
-							</div>
-							<div className="info">
-								{valor.toFixed(2).replace('.', ',')}
-							</div>
-						</div>
-						<div className="buttons">
-							<button onClick={props.closeModalRemove}>
-								{success ? "Fechar" : "Cancelar"}
-							</button>
-							<div className='apagar'>
-								<button
-									onKeyDown={handleKeyDown}
-									onClick={() => apagaMovimentacao(id)}
-									disabled={success}
-								>
-									{success 
-										? <CheckIcon sx={{color: "green"}}/>
-										: <DeleteForever sx={{color: "#B82121"}} />}
-								</button>
-							</div>
-							
-						</div>
-						<div className='progress'>
-							{loading && (
-								<Box sx={{ width: '100%' }}>
-									<LinearProgress />
-								</Box>
-							)}
+						<div className="info">
+							{valor.toFixed(2).replace('.', ',')}
 						</div>
 					</div>
 				</div>
-			)}
-		</>
+			</DialogContent>
+			<DialogActions className='modal-planejamento-butons'>
+				<Button onClick={props.closeModalRemove}>
+					{success ? "Fechar" : "Cancelar"}
+				</Button>
+				<Button
+					onKeyDown={handleKeyDown}
+					onClick={() => apagaMovimentacao(id)}
+					disabled={success}
+				>
+					Apagar
+				</Button>
+			</DialogActions>
+		</Dialog>
 	);
 
 	async function apagaMovimentacao(id: number) {
-		setLoading(true);
 		setSuccess(false);
 		const response = await movimentacaoService.apagaMovimentacao(id);
-		setLoading(false);
 		setSuccess(true);
 		props.closeModalRemove();
 		if (response?.status === 200) {
